8d9662cd0956abe156d0737cecdf4c8553d8ee47
rafiakbar13 <muhammadrafia13@gmail.com>
2024-01-05 22:13:39 +0700

    setup admin

---
+++ src/app/(admin)/dashboard/categories/page.tsx
@@ -0,0 +1,8 @@
+/*
+    - This page is for the admin to manage categories
+    - name
+    - image
+    - jumlah food yang ada di category tersebut (relasi dengan food)
+    - edit
+    - delete
+*/
---
+++ src/app/(admin)/dashboard/menu/page.tsx
@@ -0,0 +1,11 @@
+/*
+    - This page is for the admin to manage menu
+    - id
+    - name
+    - price
+    - image
+    - rating
+    - category
+    - edit
+    - delete
+*/
